16180dabfc9992b3c884427e9a6dd1a02dae1781
jbelusik <jozef.belusik@gmail.com>
2022-05-16 19:00:50 +0200

    comment for profesia

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,21 @@ import { observer } from "mobx-react";
 
 import { Header } from "./components/Header";
 import { Homepage } from "./routes/Homepage";
-
 import { createStore } from "./store/store";
 import { Route, Routes } from "react-router-dom";
 import { Board } from "./routes/Board";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+/*
+  Vec ktorú by som určite ešte zmenil je štýlovanie. Minimálne 
+  by som vyňal štýly do osobytných css súborov a v komponentoch 
+  použil className aby bol kód čitatelnejší. Ešte ideálnejšie by 
+  bolo použiť less, keďže napr. farba pre buttony by sa dala dať
+  do konštanty a potom si ju už iba importovať v jednotlivých less 
+  súboroch. 
+  Poprípade by som ešte vyskúšal Tailwind css, ale neviem či by to
+  nespôsobovalo problémy so štýlovaním material ui knižnice.
+*/
 
 export const App: React.FC = observer(() => {
   const [store] = useState(() => createStore());
